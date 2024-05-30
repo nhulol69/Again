eefe87601514e588951437172c897bba09d53ee6
Khang Hoang <hoangkhang200310@gmail.com>
2024-05-30 12:15:35 +0700

    Thay doi 1 ti o main

---
+++ src/Employee.java
@@ -0,0 +1,27 @@
+public class Employee {
+    private String name;
+    private int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
