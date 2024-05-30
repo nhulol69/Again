bcf132afb06c736363026e29d7bb2b4463d588b8
Khang Hoang <hoangkhang200310@gmail.com>
2024-05-30 12:15:00 +0700

    Thay doi 1 ti o main

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("This is first");
     }
+
+    public void display(){
+        System.out.println("Something here");
+    }
 }
